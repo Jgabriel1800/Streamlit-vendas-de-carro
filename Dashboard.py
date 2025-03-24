217b027f558246c010823623f3d5306f93f4f18c
Jgabriel1800 <136657802+Jgabriel1800@users.noreply.github.com>
2025-03-24 09:35:08 -0300

    adiciona formatação de números e exibe métricas no dashboard de vendas

--- Dashboard.py
+++ Dashboard.py
@@ -3,12 +3,25 @@ import requests
 import pandas as pd
 import plotly.express as px
 
+def formata_numero(valor,prefixo=''):
+    for unidade in ['','mil']:
+        if valor<1000:
+            return f'{prefixo} {valor:.2f} {unidade}'
+        valor /=1000
+    return f'{prefixo} {valor:.2f} milhões'
+
 st.title('DASHBOARD DE VENDAS :car:')
 
 arquivo_csv='car_ad.csv'
-
-
 dados = pd.read_csv(arquivo_csv, encoding="latin1")  
 
+#criando colunas
+coluna1,coluna2=st.columns(2)
+with coluna1:
+    st.metric('Receita',formata_numero(dados['price'].sum()))
+with coluna2:
+    st.metric('Quantidade de vendas',formata_numero(dados.shape[0]))
+
+st.dataframe(dados)
 
 
